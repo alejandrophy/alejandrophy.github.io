4865ddb972c50045a12c8d33776955b97468485c
macasatanedag <manuelal.castaneda@correo.usa.edu.co>
2018-06-28 23:27:53 -0500

    jsobn

--- ex.php
+++ ex.php
@@ -1,4 +1,15 @@
 <?php
-echo "<h3>Sorry, there was a problem.</h3>
+
+    $jsondata = array();
+
+    $jsondata['success'] = false;
+    $jsondata['message'] = 'Hola! El valor recibido no es correcto.';
+
+    
+
+    //Aunque el content-type no sea un problema en la mayoría de casos, es recomendable especificarlo
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($jsondata);
+    exit();
 
 ?>
