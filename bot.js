650724beea8268c5741badf48d29d6f7039fc892
manuel.castaneda <manuel.castaneda@pevaar.com>
2018-06-29 11:53:26 -0500

    vista js

--- bot.js
+++ bot.js
@@ -3,12 +3,23 @@ var Twit = require('twit');
 var config = require('./configure');
 var T = new Twit(config);
 
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, gotData)
+T.get('search/tweets', { q: 'Colombia since:2018-06-25', count: 1000 }, gotData)
 
 
 function gotData(err, data, response) {
 	var tweets= data.statuses;
 	for(var i=0; i<tweets.length; i++){
-  console.log(tweets[i].text)
+  console.log(i+") "+tweets[i].user.name+' : '+ tweets[i].user.location)
+  console.log('---------------------------------------')
   }
 }
+
+var stream = T.stream('statuses/filter', { track: '#Colombia', language: 'en' })
+
+stream.on('tweet', function (tweet) {
+  var tweets= tweet.statuses;
+	for(var i=0; i<tweets.length; i++){
+  console.log(i+") "+tweets[i].user.name+' : '+ tweets[i].user.location)
+  console.log('---------------------------------------')
+			}
+})
